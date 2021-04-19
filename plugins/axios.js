eddc900e7fc045328b20892dde1382eb6ef7ccd5
Rui Martins <rui.martins@sellwerk.de>
2021-04-19 16:12:12 +0100

    fixed cookie header error

--- plugins/axios.js
+++ plugins/axios.js
@@ -1,10 +1,19 @@
 export default function ({ $axios, store, redirect }) {
     $axios.onRequest(config => {
       if(store.state.user.token){
-        config.headers.cookie = null
+        if(process.server) config.headers.cookie = null
         config.headers.Authorization = 'Bearer ' + store.state.user.token
         return config
       }else{
+        redirect({name: 'index'})
+      }
+    })
+
+    $axios.onResponse(response => {
+      if(response.status.code === 401){
+        store.dispatch('user/setToken', null)
+        store.dispatch('user/setUser', null)
+        store.dispatch('user/setUserId', null)
         redirect({name: 'index'})
       }
     })
